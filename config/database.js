dbcfeb17c916edc973332a6fa0ddcc229299fa31
RitwickBhargav <47817908+RitwickBhargav@users.noreply.github.com>
2019-06-27 12:21:43 +0530

    Delete database.js

--- config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    database: "mongodb://ritwick80:ritwick123@cluster0-shard-00-00-rloq9.mongodb.net:27017,cluster0-shard-00-01-rloq9.mongodb.net:27017,cluster0-shard-00-02-rloq9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
-    secret: "mysecret"
-}
